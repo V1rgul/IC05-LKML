fe704943a4e0373e9bf2c3c14d0dabe19d18f9e4
v1rgul <v1rgul.42@gmail.com>
2015-11-17 17:38:04 +0100

    merge :D

--- scripts/tools.py
+++ scripts/tools.py
@@ -1,7 +1,7 @@
 companies = {
 	"Greg Kroah-Hartman"		: "Linux Foundation",
 	"Kamal Mostafa"				: "Canonical",
-	"Arnaldo Carvalho de Melo"	: "Red hat",
+	"Arnaldo Carvalho de Melo"	: "Red Hat",
 	"Luis Henriques"			: "Canonical",
 	"Peter Zijlstra"			: "Red Hat",
 	"Ingo Molnar"				: "Red Hat",
@@ -48,17 +48,329 @@ companies = {
 	"lizf@kernel ..."			: "",
 	"Paul Bolle"				: "Tiscali",
 
+	#"lizf@kernel ..."			: "",	#591
+	"Yinghai Lu"				: "Oracle",	#585
+	"Tomeu Vizoso"				: "Freelance",	#573
+	"Russell King - ARM Linux"	: "Deep Blue Solutions Ltd",	#572
+	"Chanwoo Choi"				: "Samsung",	#572
+	"Michal Hocko"				: "SUSE",	#569
+	"Dan Carpenter"				: "Oracle",	#565
+	"Rob Herring"				: "Linaro",	#564
+	"Denys Vlasenko"			: "Red Hat",	#543
+	"Sergey Senozhatsky"		: "Samsung",	#540
+	"Marc Zyngier"				: "ARM",	#531
+	"Dave Hansen"				: "Intel",	#520
+	"Will Deacon"				: "",	#496
+	"Adrian Hunter"				: "",	#493
+	"Richard Weinberger"		: "",	#484
+	"Bjorn Helgaas"				: "",	#481
+	"Maxime Ripard"				: "",	#479
+	"Robert Baldyga"			: "",	#468
+	"Mateusz Kulikowski"		: "",	#468
+	"Masami Hiramatsu"			: "",	#467
+	"Sascha Hauer"				: "",	#466
+	"Peter Hurley"				: "",	#462
+	"Hanjun Guo"				: "",	#462
+	"Jonathan Cameron"			: "",	#461
+	"Boris Brezillon"			: "",	#460
+	"Roger Quadros"				: "",	#454
+	"Kishon Vijay Abraham I"	: "",	#443
+	"Michael Ellerman"			: "",	#439
+	"Ian Abbott"				: "",	#435
+	"Tony Lindgren"				: "",	#434
+	"Andi Kleen"				: "",	#433
+	"Vlastimil Babka"			: "",	#431
+	"Herbert Xu"				: "",	#429
+	"Brian Norris"				: "",	#425
+	"Alexei Starovoitov"		: "",	#414
+	"Pali Rohár"				: "",	#409
+	"Nicholas Mc Guire"			: "",	#409
+	"K. Y. Srinivasan"			: "",	#409
+	"Felipe Balbi"				: "",	#409
+	"Srinivas Kandagatla"		: "",	#408
+	"John Stultz"				: "",	#408
+	"Mel Gorman"				: "",	#407
+	"Andy Shevchenko"			: "",	#407
+	"Yijing Wang"				: "",	#404
+	"Julia Lawall"				: "",	#401
+	"Mark Rutland"	: "",	#399
+	"Daniel Lezcano"	: "",	#397
+	"David Howells"	: "",	#393
+	"Josh Poimboeuf"	: "",	#387
+	"Toshi Kani"	: "",	#384
+	"Nicolas Ferre"	: "",	#382
+	"Jacek Anaszewski"	: "",	#375
+	"Joerg Roedel"	: "",	#367
+	"Thierry Reding"	: "",	#361
+	"Catalin Marinas"	: "",	#357
+	"Wolfram Sang"	: "",	#352
+	"Jiri Kosina"	: "",	#351
+	"Shuah Khan"	: "",	#348
+	"Robert Jarzmik"	: "",	#348
+	"Jens Axboe"	: "",	#346
+	"Alexey Kardashevskiy"	: "",	#342
+	"Frederic Weisbecker"	: "",	#340
+	"Stefan Agner"	: "",	#336
+	"NeilBrown"	: "",	#333
+	"Ray Jui"	: "",	#331
+	"Charles Keepax"	: "",	#329
+	"Davidlohr Bueso"	: "",	#328
+	"Takashi Iwai"	: "",	#326
+	"Sudeep Holla"	: "",	#321
+	"Maxime Coquelin"	: "",	#319
+	"Kees Cook"	: "",	#318
+	"David Rientjes"	: "",	#318
+	"Bjorn Andersson"	: "",	#314
+	"SF Markus Elfring"	: "",	#312
+	"Michael Wang"	: "",	#308
+	"Chao Yu"	: "",	#306
+	"Minchan Kim"	: "",	#304
+	"Ming Lei"	: "",	#303
+	"Sasha Levin"	: "",	#297
+	"Jaegeuk Kim"	: "",	#297
+	"Wanpeng Li"	: "",	#295
+	"Rasmus Villemoes"	: "",	#294
+	"Rickard Strandqvist"	: "",	#292
+	"Rik van Riel"	: "",	#288
+	"Fabian Frederick"	: "",	#288
+	"Mike Galbraith"	: "",	#287
+	"Darren Hart"	: "",	#281
+	"Jan Kara"	: "",	#278
+	"Antoine Tenart"	: "",	#278
+	"Andrey Ryabinin"	: "",	#276
+	"Joonsoo Kim"	: "",	#272
+	"Stephan Mueller"	: "",	#271
+	"David Vrabel"	: "",	#270
+	"Vitaly Kuznetsov"	: "",	#269
+	"Daniel Thompson"	: "",	#266
+	"Christoph Lameter"	: "",	#266
+	"Andreas Gruenbacher"	: "",	#265
+	"christophe leroy"	: "",	#262
+	"Vinod Koul"	: "",	#261
+	"Sukadev Bhattiprolu"	: "",	#261
+	"Matt Fleming"	: "",	#261
+	"Vladimir Davydov"	: "",	#259
+	"Sebastian Reichel"	: "",	#259
+	"Murali Karicheri"	: "",	#258
+	"Vaibhav Hiremath"	: "",	#257
+	"Juergen Gross"	: "",	#257
+	"Peter Ujfalusi"	: "",	#256
+	"Lv Zheng"	: "",	#256
+	"Masahiro Yamada"	: "",	#255
+	"Luis de Bethencourt"	: "",	#253
+	"Ricardo Ribalda Delgado"	: "",	#251
+	"Bartlomiej Zolnierkiewicz"	: "",	#250
+	"Eric Auger"	: "",	#249
+	"Johan Hovold"	: "",	#248
+	"Ulf Hansson"	: "",	#247
+	"Benjamin Tissoires"	: "",	#247
+	"Alexander Kuleshov"	: "",	#245
+	"Waiman Long"	: "",	#244
+	"Peter Griffin"	: "",	#243
+	"Dave Chinner"	: "",	#241
+	"Vineet Gupta"	: "",	#240
+	"Rusty Russell"	: "",	#239
+	"kbuild test robot"	: "",	#235
+	"Stas Sergeev"	: "",	#232
+	"Scott Wood"	: "",	#232
+	"Chris Metcalf"	: "",	#231
+	"Yoshinori Sato"	: "",	#230
+	"Sergei Shtylyov"	: "",	#228
+	"Paul Gortmaker"	: "",	#227
+	"Michal Simek"	: "",	#227
+	"Daniel Vetter"	: "",	#225
+	"David Ahern"	: "",	#224
+	"Eduardo Valentin"	: "",	#223
+	"Alan Stern"	: "",	#223
+	"Morten Rasmussen"	: "",	#218
+	"Florian Fainelli"	: "",	#216
+	"Cyrille Pitchen"	: "",	#216
+	"Matthias Brugger"	: "",	#214
+	"Markus Pargmann"	: "",	#214
+	"Alex Williamson"	: "",	#213
+	"green@linuxhac ..."	: "",	#212
+	"Olof Johansson"	: "",	#210
+	"Konstantin Khlebnikov"	: "",	#210
+	"tip-bot for Arnaldo Carvalho de Melo"	: "",	#208
+	"Mathieu Poirier"	: "",	#207
+	"Petr Mladek"	: "",	#203
+	"Jonathan Corbet"	: "",	#203
+	"Andy Gross"	: "",	#200
+	"Daniel Baluta"	: "",	#199
+	"Feng Wu"	: "",	#198
+	"Eric Dumazet"	: "",	#198
+	"Lorenzo Pieralisi"	: "",	#197
+	"Lad Prabhakar"	: "",	#196
+	"Ivan T. Ivanov"	: "",	#196
+	"Sedat Dilek"	: "",	#194
+	"Jisheng Zhang"	: "",	#194
+	"Alexander Shishkin"	: "",	#194
+	"huang ying"	: "",	#193
+	"Baoquan He"	: "",	#193
+	""	: "",	#193
+	"Azael Avalos"	: "",	#192
+	"Boaz Harrosh"	: "",	#190
+	"Vivien Didelot"	: "",	#189
+	"Michael Kerrisk (man-pages)"	: "",	#189
+	"Vikas Shivappa"	: "",	#187
+	"Chen-Yu Tsai"	: "",	#187
+	"Javi Merino"	: "",	#186
+	"Tom Zanussi"	: "",	#185
+	"Julien Grall"	: "",	#185
+	"H. Peter Anvin"	: "",	#183
+	"Willy Tarreau"	: "",	#182
+	"tip-bot for Jiri Olsa"	: "",	#182
+	"Stephane Eranian"	: "",	#182
+	"Paul Burton"	: "",	#181
+	"Naoya Horiguchi"	: "",	#180
+	"Boris Ostrovsky"	: "",	#180
+	"Tomas Winkler"	: "",	#179
+	"Richard Cochran"	: "",	#178
+	"Mathieu Desnoyers"	: "",	#178
+	"Andrew Bresticker"	: "",	#178
+	"Mike Kravetz"	: "",	#176
+	"Yaniv Gardi"	: "",	#175
+	"Jeff Layton"	: "",	#174
+	"Suzuki K. Poulose"	: "",	#173
+	"He Kuang"	: "",	#171
+	"Xunlei Pang"	: "",	#170
+	"Sebastian Andrzej Siewior"	: "",	#169
+	"Finn Thain"	: "",	#169
+	"Radim Krčmář"	: "",	#168
+	"Doug Anderson"	: "",	#168
+	"Paul Moore"	: "",	#165
+	"Yakir Yang"	: "",	#164
+	"Rhyland Klein"	: "",	#163
+	"Eric Anholt"	: "",	#163
+	"Shawn Lin"	: "",	#162
+	"Valentin Rothberg"	: "",	#161
+	"Frans Klaver"	: "",	#161
+	"(Eric W. Biederman)"	: "",	#161
+	"tip-bot for Thomas Gleixner"	: "",	#160
+	"Mika Westerberg"	: "",	#159
+	"Heiko Stübner"	: "",	#157
+	"Ezequiel Garcia"	: "",	#157
+	"Nicholas A. Bellinger"	: "",	#156
+	"Preeti U Murthy"	: "",	#155
+	"Alexander Holler"	: "",	#154
+	"KY Srinivasan"	: "",	#153
+	"Dan Streetman"	: "",	#153
+	"Theodore Tso"	: "",	#152
+	"Xiao Guangrong"	: "",	#151
+	"Stephen Warren"	: "",	#151
+	"Laurent Pinchart"	: "",	#151
+	"Heikki Krogerus"	: "",	#151
+	"Daniel Kurtz"	: "",	#151
+	"Alexandre Courbot"	: "",	#150
+	"Ross Zwisler"	: "",	#149
+	"Michal Marek"	: "",	#149
+	"Lars-Peter Clausen"	: "",	#149
+	"Johannes Weiner"	: "",	#148
+	"David Herrmann"	: "",	#142
+	"David Daney"	: "",	#142
+	"Kalle Valo"	: "",	#141
+	"Irina Tirdea"	: "",	#141
+	"Wenyou Yang"	: "",	#139
+	"Ming Lin"	: "",	#139
+	"kan.liang@intel ..."	: "",	#139
+	"Zubair Lutfullah Kakakhel"	: "",	#138
+	"Andrzej Hajda"	: "",	#138
+	"Vince Weaver"	: "",	#136
+	"tip-bot for Jiang Liu"	: "",	#136
+	"Josh Triplett"	: "",	#136
+	"Caesar Wang"	: "",	#136
+	"Peter Chen"	: "",	#135
+	"Brian Gerst"	: "",	#135
+	"Uwe Kleine-König"	: "",	#134
+	"Sebastian Hesselbarth"	: "",	#134
+	"Gregory CLEMENT"	: "",	#134
+	"Eric B Munson"	: "",	#134
+	"Konrad Rzeszutek Wilk"	: "",	#133
+	"Jean Delvare"	: "",	#133
+	"Alex Bennée"	: "",	#133
+	"Shawn Guo"	: "",	#131
+	"Kevin Hilman"	: "",	#131
+	"Jarkko Sakkinen"	: "",	#130
+	"tip-bot for Adrian Hunter"	: "",	#129
+	"Jan Kiszka"	: "",	#129
+	"Joe Stringer"	: "",	#128
+	"Jason Low"	: "",	#128
+	"David Gibson"	: "",	#128
+	"Wangnan (F)"	: "",	#127
+	"Tomi Valkeinen"	: "",	#127
+	"Richard Fitzgerald"	: "",	#127
+	"Vaishali Thakkar"	: "",	#125
+	"Michael Turquette"	: "",	#125
+	"Martin Kepplinger"	: "",	#124
+	"Andrew Lunn"	: "",	#124
+	"tip-bot for Andy Lutomirski"	: "",	#123
+	"Randy Dunlap"	: "",	#123
+	"Xishi Qiu"	: "",	#121
+	"Aleksa Sarai"	: "",	#121
+	"Richard Guy Briggs"	: "",	#120
+	"Philipp Zabel"	: "",	#120
+	"Peter Hung"	: "",	#120
+	"One Thousand Gnomes"	: "",	#120
+	"Nishanth Menon"	: "",	#120
+	"Eddie Huang"	: "",	#119
+	"Pantelis Antoniou"	: "",	#118
+	"James Bottomley"	: "",	#118
+	"Chen Gang"	: "",	#117
+	"Ard Biesheuvel"	: "",	#117
+	"Yingjoe Chen"	: "",	#116
+	"James Hogan"	: "",	#116
+	"Fabio Estevam"	: "",	#116
+	"Ludovic Desroches"	: "",	#115
+	"Fengguang Wu"	: "",	#115
+	"Suravee Suthikulpanit"	: "",	#114
+	"Mike Snitzer"	: "",	#114
+	"Mike Rapoport"	: "",	#114
+	"Baolin Wang"	: "",	#114
+	"Paul Walmsley"	: "",	#113
+	"Casey Schaufler"	: "",	#113
+	"Minfei Huang"	: "",	#112
+	"Kukjin Kim"	: "",	#112
+	"Anshuman Khandual"	: "",	#112
+	"Michael Mueller"	: "",	#111
+	"Daniel Wagner"	: "",	#111
+	"Austin S Hemmelgarn"	: "",	#111
+	"Jeff Moyer"	: "",	#110
+	"Dexuan Cui"	: "",	#110
+	"Moritz Fischer"	: "",	#109
+	"Marcelo Tosatti"	: "",	#108
+	"Dmitry Kalinkin"	: "",	#108
+	"Alexey Dobriyan"	: "",	#108
+	"Jason Gunthorpe"	: "",	#107
+	"Aravind Gopalakrishnan"	: "",	#107
+	"Kumar Gala"	: "",	#105
+	"Josh Boyer"	: "",	#105
+	"Wang Long"	: "",	#104
+	"Vignesh R"	: "",	#104
+	"Andrea Arcangeli"	: "",	#104
+	"Hugh Dickins"	: "",	#103
+	"Hans de Goede"	: "",	#103
+	"Benjamin Herrenschmidt"	: "",	#103
+	"Timur Tabi"	: "",	#102
+	"tip-bot for Denys Vlasenko"	: "",	#101
+	"Sreekanth Reddy"	: "",	#101
+	"Ramneek Mehresh"	: "",	#101
+	"Madhavan Srinivasan"	: "",	#101
+	"Liang Kan"	: "",	#101
+	"Juston Li"	: "",	#101
+	"santosh shilimkar"	: "",	#100
+	"Namjae Jeon"	: ""	#100
 
 
-	"Sasha Levin"				: "Oracle",
-	"Aleksa Sarai"				: "University of Sydney",
-	"Alexei Starovoitov"		: "PLUMgrid",
-	"Frank Rowand"				: "Sony",
-	"Jan Kara"					: "SUSE",
-	"Masami Hiramatsu"			: "Hitachi",
-	"He Kuang"					: "IBM",
-	"David Ahern"				: "Cumulus Networks",
-	"Andi Kleen"				: "Intel"
+
+	# "Sasha Levin"				: "Oracle",
+	# "Aleksa Sarai"				: "University of Sydney",
+	# "Alexei Starovoitov"		: "PLUMgrid",
+	# "Frank Rowand"				: "Sony",
+	# "Jan Kara"					: "SUSE",
+	# "Masami Hiramatsu"			: "Hitachi",
+	# "He Kuang"					: "IBM",
+	# "David Ahern"				: "Cumulus Networks",
+	# "Andi Kleen"				: "Intel"
 
 
 }
